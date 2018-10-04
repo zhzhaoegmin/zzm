ef2842e3d266a18b645544b11770350aa5632d7d
zhzhaoegmin <32921181+zhzhaoegmin@users.noreply.github.com>
2018-10-03 21:02:01 -0700

    Add files via upload

---
+++ animal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Animal
+{
+protected:
+    int age;
+    char name[10];
+public:
+    void set_data (int a, char b[10])
+    {
+        age = a;
+        strcpy(name,b);
+    }
+    
+};
+
+class Zebra:public Animal
+{public:
+    
+    void message_zebra()
+    {cout<< "The zebra named "<< name<<" is "<< age << " years old. The zebra comes from Africa. \n";}
+};
+
+class Dolphin: public Animal
+{public:
+    
+    void message_dolphin()
+    {cout<< "The dolphin named "<< name<< " is "<<age << " years old. The dolphin comes from New Zeland.\n";}
+};
+
+
+int main ()
+{
+    Zebra zeb;
+    Dolphin dol;
+    char n1[10]="Diana";
+    char n2[10]="Cool";
+    
+    zeb.set_data (5,n1);
+    dol.set_data (2,n2);
+    
+    zeb.message_zebra() ;
+    dol.message_dolphin() ;
+    
+    return 0;
+}
